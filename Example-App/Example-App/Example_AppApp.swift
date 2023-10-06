a495051e1c0981d0d695b0f67a72caa1dde60c75
Egzon Arifi <EgzonArifi@users.noreply.github.com>
2023-10-06 10:52:43 +0200

    Add Example-App (#6)

---
+++ Example-App/Example-App/Example_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Example_AppApp.swift
+//  Example-App
+//
+//  Created by Egzon Arifi on 06/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Example_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
